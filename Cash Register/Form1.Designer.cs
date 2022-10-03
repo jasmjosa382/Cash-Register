5b8e7c2f5672debdd512e05594a07f9d7cc8a905
jasmjosa382 <jasmjosa382@ed.amdsb.ca>
2022-10-03 10:14:27 -0400

    sync

--- Cash Register/Form1.Designer.cs
+++ Cash Register/Form1.Designer.cs
@@ -70,7 +70,7 @@
             this.coffeeLabel.AutoSize = true;
             this.coffeeLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.coffeeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.coffeeLabel.Location = new System.Drawing.Point(0, 87);
+            this.coffeeLabel.Location = new System.Drawing.Point(0, 77);
             this.coffeeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.coffeeLabel.Name = "coffeeLabel";
             this.coffeeLabel.Size = new System.Drawing.Size(152, 23);
@@ -82,7 +82,7 @@
             this.pasteryLabel.AutoSize = true;
             this.pasteryLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pasteryLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.pasteryLabel.Location = new System.Drawing.Point(0, 133);
+            this.pasteryLabel.Location = new System.Drawing.Point(0, 117);
             this.pasteryLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pasteryLabel.Name = "pasteryLabel";
             this.pasteryLabel.Size = new System.Drawing.Size(172, 23);
@@ -94,7 +94,7 @@
             this.sandwichLabel.AutoSize = true;
             this.sandwichLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.sandwichLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.sandwichLabel.Location = new System.Drawing.Point(0, 181);
+            this.sandwichLabel.Location = new System.Drawing.Point(0, 157);
             this.sandwichLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.sandwichLabel.Name = "sandwichLabel";
             this.sandwichLabel.Size = new System.Drawing.Size(193, 23);
@@ -103,24 +103,24 @@
             // 
             // drinkInput
             // 
-            this.drinkInput.Location = new System.Drawing.Point(272, 91);
-            this.drinkInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.drinkInput.Location = new System.Drawing.Point(272, 78);
+            this.drinkInput.Margin = new System.Windows.Forms.Padding(4);
             this.drinkInput.Name = "drinkInput";
             this.drinkInput.Size = new System.Drawing.Size(89, 22);
             this.drinkInput.TabIndex = 4;
             // 
             // pasteryInput
             // 
-            this.pasteryInput.Location = new System.Drawing.Point(272, 133);
-            this.pasteryInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pasteryInput.Location = new System.Drawing.Point(272, 119);
+            this.pasteryInput.Margin = new System.Windows.Forms.Padding(4);
             this.pasteryInput.Name = "pasteryInput";
             this.pasteryInput.Size = new System.Drawing.Size(89, 22);
             this.pasteryInput.TabIndex = 5;
             // 
             // sandwichInput
             // 
-            this.sandwichInput.Location = new System.Drawing.Point(272, 181);
-            this.sandwichInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.sandwichInput.Location = new System.Drawing.Point(272, 159);
+            this.sandwichInput.Margin = new System.Windows.Forms.Padding(4);
             this.sandwichInput.Name = "sandwichInput";
             this.sandwichInput.Size = new System.Drawing.Size(89, 22);
             this.sandwichInput.TabIndex = 6;
@@ -131,8 +131,8 @@
             this.totalButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.totalButton.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.totalButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.totalButton.Location = new System.Drawing.Point(91, 229);
-            this.totalButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.totalButton.Location = new System.Drawing.Point(90, 203);
+            this.totalButton.Margin = new System.Windows.Forms.Padding(4);
             this.totalButton.Name = "totalButton";
             this.totalButton.Size = new System.Drawing.Size(235, 43);
             this.totalButton.TabIndex = 7;
@@ -145,7 +145,7 @@
             this.subtotalLabel.AutoSize = true;
             this.subtotalLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.subtotalLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.subtotalLabel.Location = new System.Drawing.Point(0, 289);
+            this.subtotalLabel.Location = new System.Drawing.Point(0, 271);
             this.subtotalLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.subtotalLabel.Name = "subtotalLabel";
             this.subtotalLabel.Size = new System.Drawing.Size(79, 23);
@@ -157,7 +157,7 @@
             this.taxLabel.AutoSize = true;
             this.taxLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.taxLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.taxLabel.Location = new System.Drawing.Point(0, 330);
+            this.taxLabel.Location = new System.Drawing.Point(0, 314);
             this.taxLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.taxLabel.Name = "taxLabel";
             this.taxLabel.Size = new System.Drawing.Size(45, 23);
@@ -169,7 +169,7 @@
             this.totalLabel.AutoSize = true;
             this.totalLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.totalLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.totalLabel.Location = new System.Drawing.Point(0, 375);
+            this.totalLabel.Location = new System.Drawing.Point(0, 353);
             this.totalLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.totalLabel.Name = "totalLabel";
             this.totalLabel.Size = new System.Drawing.Size(53, 23);
@@ -179,24 +179,24 @@
             // subtotalOutput
             // 
             this.subtotalOutput.BackColor = System.Drawing.SystemColors.Window;
-            this.subtotalOutput.Location = new System.Drawing.Point(155, 293);
-            this.subtotalOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.subtotalOutput.Location = new System.Drawing.Point(155, 273);
+            this.subtotalOutput.Margin = new System.Windows.Forms.Padding(4);
             this.subtotalOutput.Name = "subtotalOutput";
             this.subtotalOutput.Size = new System.Drawing.Size(132, 22);
             this.subtotalOutput.TabIndex = 11;
             // 
             // taxOutput
             // 
-            this.taxOutput.Location = new System.Drawing.Point(155, 334);
-            this.taxOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.taxOutput.Location = new System.Drawing.Point(155, 314);
+            this.taxOutput.Margin = new System.Windows.Forms.Padding(4);
             this.taxOutput.Name = "taxOutput";
             this.taxOutput.Size = new System.Drawing.Size(132, 22);
             this.taxOutput.TabIndex = 12;
             // 
             // totalOutput
             // 
-            this.totalOutput.Location = new System.Drawing.Point(155, 379);
-            this.totalOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.totalOutput.Location = new System.Drawing.Point(155, 355);
+            this.totalOutput.Margin = new System.Windows.Forms.Padding(4);
             this.totalOutput.Name = "totalOutput";
             this.totalOutput.Size = new System.Drawing.Size(132, 22);
             this.totalOutput.TabIndex = 13;
@@ -205,7 +205,7 @@
             // 
             this.separationLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.separationLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.separationLabel.Location = new System.Drawing.Point(1, 420);
+            this.separationLabel.Location = new System.Drawing.Point(1, 398);
             this.separationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.separationLabel.Name = "separationLabel";
             this.separationLabel.Size = new System.Drawing.Size(404, 16);
@@ -217,7 +217,7 @@
             this.tenderedLabel.AutoSize = true;
             this.tenderedLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tenderedLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.tenderedLabel.Location = new System.Drawing.Point(0, 455);
+            this.tenderedLabel.Location = new System.Drawing.Point(0, 436);
             this.tenderedLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.tenderedLabel.Name = "tenderedLabel";
             this.tenderedLabel.Size = new System.Drawing.Size(86, 23);
@@ -226,8 +226,8 @@
             // 
             // tenderedInput
             // 
-            this.tenderedInput.Location = new System.Drawing.Point(155, 455);
-            this.tenderedInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tenderedInput.Location = new System.Drawing.Point(155, 438);
+            this.tenderedInput.Margin = new System.Windows.Forms.Padding(4);
             this.tenderedInput.Name = "tenderedInput";
             this.tenderedInput.Size = new System.Drawing.Size(132, 22);
             this.tenderedInput.TabIndex = 16;
@@ -238,8 +238,8 @@
             this.tenderedButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.tenderedButton.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tenderedButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.tenderedButton.Location = new System.Drawing.Point(104, 501);
-            this.tenderedButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tenderedButton.Location = new System.Drawing.Point(104, 479);
+            this.tenderedButton.Margin = new System.Windows.Forms.Padding(4);
             this.tenderedButton.Name = "tenderedButton";
             this.tenderedButton.Size = new System.Drawing.Size(221, 34);
             this.tenderedButton.TabIndex = 17;
@@ -252,7 +252,7 @@
             this.changeLabel.AutoSize = true;
             this.changeLabel.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.changeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.changeLabel.Location = new System.Drawing.Point(0, 564);
+            this.changeLabel.Location = new System.Drawing.Point(0, 534);
             this.changeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.changeLabel.Name = "changeLabel";
             this.changeLabel.Size = new System.Drawing.Size(69, 23);
@@ -261,8 +261,8 @@
             // 
             // tenderedOutput
             // 
-            this.tenderedOutput.Location = new System.Drawing.Point(155, 567);
-            this.tenderedOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tenderedOutput.Location = new System.Drawing.Point(155, 534);
+            this.tenderedOutput.Margin = new System.Windows.Forms.Padding(4);
             this.tenderedOutput.Name = "tenderedOutput";
             this.tenderedOutput.Size = new System.Drawing.Size(132, 22);
             this.tenderedOutput.TabIndex = 19;
@@ -273,23 +273,24 @@
             this.receiptButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.receiptButton.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.receiptButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.receiptButton.Location = new System.Drawing.Point(5, 620);
-            this.receiptButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.receiptButton.Location = new System.Drawing.Point(4, 576);
+            this.receiptButton.Margin = new System.Windows.Forms.Padding(4);
             this.receiptButton.Name = "receiptButton";
             this.receiptButton.Size = new System.Drawing.Size(400, 34);
             this.receiptButton.TabIndex = 20;
             this.receiptButton.Text = "Print Receipt";
             this.receiptButton.UseVisualStyleBackColor = false;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
             // 
             // receiptOutput
             // 
             this.receiptOutput.BackColor = System.Drawing.Color.White;
-            this.receiptOutput.Location = new System.Drawing.Point(435, 69);
+            this.receiptOutput.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptOutput.Location = new System.Drawing.Point(490, 92);
             this.receiptOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.receiptOutput.Name = "receiptOutput";
-            this.receiptOutput.Size = new System.Drawing.Size(345, 537);
+            this.receiptOutput.Size = new System.Drawing.Size(333, 475);
             this.receiptOutput.TabIndex = 21;
-            this.receiptOutput.Text = "Moonshine Cafe";
             // 
             // Form1
             // 
@@ -320,7 +321,7 @@
             this.Controls.Add(this.coffeeLabel);
             this.Controls.Add(this.titleLabel);
             this.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Cash Register";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Cash Register/Form1.cs
+++ Cash Register/Form1.cs
@@ -7,16 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading; 
 
 namespace Cash_Register
 {
     public partial class Form1 : Form
     {
-        double drinkPrice = 4.50;
+        double drinkPrice = 3.55;
         double numOfDrinks = 0;
         double pasteryPrice = 2.75;
         double numOfPasteries = 0;
-        double sandwichPrice = 4.50;
+        double sandwichPrice = 4.25;
         double numOfSandwiches = 0;
         double taxRate = 0.13;
         double subtotal = 0;
@@ -73,9 +74,61 @@ namespace Cash_Register
 
             changeRequired = tenderedAmount - totalPrice;
 
-           tenderedOutput.Text = $"{tenderedAmount.ToString("C")}";
+           tenderedOutput.Text = $"{changeRequired.ToString("C")}";
 
 
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            String cafeName = "Moonshine Cafe";
+            receiptOutput.Text = cafeName;
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\n\n\nOrder Number 1076";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\nSeptember 30, 2022";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\n\n\n    Drinks x{numOfDrinks} @ ${drinkPrice}";
+            Refresh();
+            Thread.Sleep(700);
+           
+            receiptOutput.Text += $"\n Pasteries x{numOfPasteries} @ ${pasteryPrice}";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\nSandwiches x{numOfSandwiches} @ ${sandwichPrice}";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\n\n\nSubtotal        {subtotal.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\nTax             {taxAmount.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\nTotal           {totalPrice.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\n\n\nTendered        {tenderedAmount.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\nChange          {changeRequired.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(700);
+
+            receiptOutput.Text += $"\n\n\nThank You For Eating With Us Today! Have A Nice Day! :)";
+            Refresh();
+            Thread.Sleep(700);
         }
     }
 }
